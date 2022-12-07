94f5625196be3840b8e67c1ad265a32f1594fd1c
‘BZBY’ <B_Z_B_Y@qq.com>
2022-12-07 08:31:11 +0800

    1

--- basic/src/me/spring/entity/TableInfo.java
+++ basic/src/me/spring/entity/TableInfo.java
@@ -1,13 +1,55 @@
 package me.spring.entity;
 
+import java.security.MessageDigest;
+
+
+
+
+
 public class TableInfo {
 	public String tableName;
 
 	public String getTableName() {
 		return tableName;
 	}
-
 	public void setTableName(String tableName) {
 		this.tableName = tableName;
 	}
+	
+	
+	 /*
+      SHA1实现  
+     */
+    public static String shaEncode(String inStr) throws Exception {
+        MessageDigest sha = null;
+        try {
+            sha = MessageDigest.getInstance("SHA");
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            e.printStackTrace();
+            return "";
+        }
+ 
+        byte[] byteArray = inStr.getBytes("UTF-8");
+        byte[] md5Bytes = sha.digest(byteArray);
+        StringBuffer hexValue = new StringBuffer();
+        for (int i = 0; i < md5Bytes.length; i++) {
+            int val = ((int) md5Bytes[i]) & 0xff;
+            if (val < 16) {
+                hexValue.append("0");
+            }
+            hexValue.append(Integer.toHexString(val));
+        }
+        return hexValue.toString();
+    }
+ 
+    public static void main(String args[]) throws Exception {
+        String str = new String("amigoxiexiexingxing");
+        System.out.println("原始：" + str);
+        System.out.println("SHA后：" + shaEncode(str));
+    }
+	
+	
+
+	
 }
